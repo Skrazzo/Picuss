2f6257c5510459152b298f28be2d3f9a59c6c91e
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-14 15:20:44 +0300

    Checkpoint
    Making tag management system

--- resources/js/Pages/ManageTags.jsx
+++ resources/js/Pages/ManageTags.jsx
@@ -3,21 +3,22 @@ import AuthLayout from './Layouts/AuthLayout'
 import { Button, Container, Flex, Input } from '@mantine/core';
 import TitleWithIcon from './Components/TitleWithIcon';
 import { IconTags } from '@tabler/icons-react';
+import TagList from './Components/TagList';
 
 
 export default function ManageTags({ auth, tags }) {
-    console.log(tags);
-
     return (
         <AuthLayout auth={auth}>
             <Container size={'md'} py={'md'} >
-                <TitleWithIcon title='Manage your tags' order={3} icon={<IconTags  size={28} strokeWidth={1.5}/>} my={16}/>
+                <TitleWithIcon title={`Manage your ${tags.length} tags`} order={3} icon={<IconTags  size={28} strokeWidth={1.5}/>} my={16}/>
                 
                 <Flex gap={8}>
                     <Input.Wrapper label="" description="Search or create new tags" error="" w={'100%'}>
-                        <Input placeholder="Tag name" />
+                        <Input placeholder="Search tag name" />
                     </Input.Wrapper>
                 </Flex>
+
+                <TagList tags={tags}/>
             </Container>
         </AuthLayout>
     )

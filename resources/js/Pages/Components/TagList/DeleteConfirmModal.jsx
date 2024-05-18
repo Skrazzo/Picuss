f8f37609174b8995f84ae86811760185d67075e9
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-05-18 22:51:19 +0300

    Made deletion confirm modal

--- resources/js/Pages/Components/TagList/DeleteConfirmModal.jsx
+++ resources/js/Pages/Components/TagList/DeleteConfirmModal.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import sty from '../../../../scss/DeleteConfirmModel.module.scss';
-import { Text, Transition } from '@mantine/core';
-export default function DeleteConfirmModal({ opened, close, title = 'Confirmation' }) {
+import { Alert, Button, Checkbox, CloseButton, Fieldset, Flex, Paper, Switch, Text, Transition } from '@mantine/core';
+import { IconInfoCircle, IconTrash } from '@tabler/icons-react';
+export default function DeleteConfirmModal({ opened, close, tags = [] }) {
     const [modalOpened, setModalOpened] = useState(false);
+    const [confirm, setConfirm] = useState(false);
+
+    function onDeleteHandler() {
+        if (!confirm) {
+            setConfirm(true);
+            return ;
+        }
+
+        // Deletion has been confirmed
+        console.log(tags);
+    }
     
     useEffect(() => {
         if (opened) {
             setTimeout(() => setModalOpened(true), 100);
+            setConfirm(false);
         }else{
             setModalOpened(false);
         }
@@ -20,16 +33,36 @@ export default function DeleteConfirmModal({ opened, close, title = 'Confirmatio
         >
             {(styles) =>
 
-                <div style={styles} onClick={close} className={sty.overlay}>
+                <div style={styles} className={sty.overlay}>
                     <Transition
                         mounted={modalOpened}
-                        transition={'fade-down'}
-                        duration={300}
+                        transition={'pop'}
+                        duration={200}
                     >
                         {(styles) => 
                             <div style={styles} className={sty.modal}>
                                 <div className={sty.nav}>
-                                    <Text c={'red'}>{title}</Text>
+                                    <div>
+                                        <div className={sty.icon}>
+                                            <IconTrash size={36}/>
+                                        </div>
+                                    </div>
+
+                                    <div className={sty.content}>
+                                        <p className={sty.label}>Delete Tags</p>
+                                        <p className={sty.description}>Are you sure you want to delete these tags? If you delete these tags, pictures that belong to them will get deleted aswell</p>
+                                    </div>
+                                </div>
+
+                                <div className={sty.footer}>
+                                    <Button 
+                                        onClick={onDeleteHandler} 
+                                        variant={(confirm) ? 'outline' : 'light'} 
+                                        color='red' 
+                                    >{(confirm) ? 'Are you sure?' : 'Delete tags'}</Button>
+
+                                    <Button onClick={close} variant={'subtle'} color='red'>Cancel</Button>
+                                    
                                 </div>
                             </div>
                         }

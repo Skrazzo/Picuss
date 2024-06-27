ae76281d7eefd971c8b59663e5f6f234d3bfc861
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-06-27 15:59:27 +0300

    Custom image lazy load

--- resources/js/Pages/Components/DashboardImage.jsx
+++ resources/js/Pages/Components/DashboardImage.jsx
@@ -4,46 +4,24 @@ import React, { useEffect, useMemo, useState } from "react";
 import sty from "../../../scss/Dashboard.module.scss";
 
 export default function DashboardImage({ image }) {
-    const picussVarName = "picuss-image-cache";
     const [blob, setBlob] = useState(null);
-    const imageQueue = 6; // allowed downloading images at the same time
-
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
 
     useEffect(() => {
-        let isActive = true;
-        const loadImage = async () => {
-            if (!window.imageQueue) window.imageQueue = [];
+        const controller = new AbortController();
 
-            while (window.imageQueue.length >= imageQueue) {
-                await sleep(1000);
-                console.log("sleeping");
-            }
-
-            // after loop is exited, whe need to check if the page is still active
-            if (!isActive) return;
-
-            window.imageQueue.push(1);
-
-            axios
-                .get(route("get.image", image.id), {
-                    responseType: "blob",
-                })
-                .then((res) => {
-                    const imageURL = URL.createObjectURL(res.data);
-                    setBlob(imageURL);
-                    window.imageQueue.pop();
-                })
-                .catch((err) => console.error(err));
-        };
-
-        loadImage();
+        axios
+            .get(route("get.image", image.id), {
+                signal: controller.signal,
+                responseType: "blob",
+            })
+            .then((res) => {
+                const imageURL = URL.createObjectURL(res.data);
+                setBlob(imageURL);
+            })
+            .catch((err) => console.error(err));
 
         return () => {
-            isActive = false;
-            window.imageQueue = [];
+            controller.abort();
         };
     }, []);
 

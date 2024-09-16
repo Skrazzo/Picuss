55da346011811a80d311e3f0c0b2ed93348660fc
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-09-16 15:20:02 +0300

    Acount deletion

--- resources/js/Pages/Components/Settings/General.jsx
+++ resources/js/Pages/Components/Settings/General.jsx
@@ -2,6 +2,26 @@ import { Chip, Fieldset, Paper } from "@mantine/core";
 import { useEffect, useState } from "react";
 import checkDarkMode from "../../Functions/checkDarkMode";
 import { useMediaQuery } from "@mantine/hooks";
+import { Button, Input, Text } from "@mantine/core";
+import { IconFileShredder } from "@tabler/icons-react";
+import "../../../../scss/Settings/General.scss";
+
+const FormInput = ({ useForm, name, label = "", placeholder = "", type = "text" }) => {
+    let inputLabel = label;
+    if (useForm.hasErrors) inputLabel = useForm.errors[name];
+
+    return (
+        <Input.Wrapper label={inputLabel} c={useForm.errors[name] ? "red" : ""}>
+            <Input
+                error={useForm.errors[name]}
+                value={useForm.data[name]}
+                onChange={(e) => useForm.setData(name, e.target.value)}
+                placeholder={placeholder}
+                type={type}
+            />
+        </Input.Wrapper>
+    );
+};
 
 export default function General() {
     const [darkMode, setDarkMode] = useState(checkDarkMode());
@@ -25,6 +45,27 @@ export default function General() {
                 <Chip onChange={(e) => setDarkMode(e)} checked={darkMode}>
                     Dark mode
                 </Chip>
+            </Fieldset>
+
+            <Fieldset
+                // legend={"Delete your account"}
+                mt={16}
+                p={16}
+                // c={"red"}
+                className="danger-fieldset"
+            >
+                <Text size="xl" mb={8}>
+                    Delete your account
+                </Text>
+                <Text>
+                    If you delete your account, all of your data will be permanently deleted. And
+                    all the pictures you uploaded will be permanently lost, as well as your shared
+                    links.
+                </Text>
+
+                <Button color="red" leftSection={<IconFileShredder {...iconProps} />} mt={24}>
+                    Delete account
+                </Button>
             </Fieldset>
         </Paper>
     );

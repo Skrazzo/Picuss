352867deb761b28b0706404a8d5a8c4264838f47
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-10-21 15:21:13 +0300

    Checkpoint

--- resources/js/Pages/Components/UserStats.jsx
+++ resources/js/Pages/Components/UserStats.jsx
@@ -1,9 +1,41 @@
 import { SimpleGrid, Skeleton } from "@mantine/core";
 import DisabledInputInfo from "./DisabledInputInfo";
 import { IconBrandOnedrive, IconPhoto, IconTag, IconTags, IconUpload, IconUser } from "@tabler/icons-react";
+import React, { useEffect, useState } from "react";
 
 export default function UserStats({ data }) {
-    console.log(data);
+    const [usedSpace, setUsedSpace] = useState(0);
+
+    // Create text for used space
+    useEffect(() => {
+        // `${data.disk_usage > 1024 ? data.disk_usage / 1024 : data.disk_usage} of ${data.user_limit} GB`
+        console.log(data);
+        if (data === null) return;
+        console.log("asd");
+        let measurement = "MB";
+        if (data.disk_usage > 1024) {
+            measurement = "GB";
+        }
+
+        // ROUND up
+        // TODO: Fix this showing incorrect data
+        // Starting with disk_usage is nan, because its not a number but a 300 MB string
+        let tmpSpace = data.disk_usage;
+        if (measurement === "MB") {
+            tmpSpace = Math.round(data.disk_usage * 100) / 100;
+        } else {
+            tmpSpace = Math.round((data.disk_usage / 1024) * 100) / 100;
+        }
+
+        if (data.user_limit !== null && data.user_limit !== 0) {
+            console.log(`${tmpSpace} of ${data.user_limit} ${measurement}`);
+            setUsedSpace(`${tmpSpace} of ${data.user_limit} ${measurement}`);
+        } else {
+            console.log("asd123");
+
+            setUsedSpace(`${tmpSpace} ${measurement}`);
+        }
+    }, [data]);
 
     const iconProps = {
         size: 20,
@@ -42,7 +74,7 @@ export default function UserStats({ data }) {
                     <DisabledInputInfo
                         tooltip={"Used storage on the server"}
                         icon={<IconBrandOnedrive {...iconProps} />}
-                        value={`${data.disk_usage} of ${data.user_limit} GB`}
+                        value={usedSpace}
                     />
                 </>
             ) : (

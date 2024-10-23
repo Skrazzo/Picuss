1f832809b9d2bb8d730aac6b814860870e7fd9b3
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-10-23 10:45:39 +0300

    Authentication test

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Can visit the website", () => {
-    it("Visits the website", () => {
-        cy.visit("/");
-
-        it("Has a login button", () => {
-            cy.get("button[data-with-left-section='true']").should("be.visible").should("have.text", "Login");
-        });
-    });
-});
